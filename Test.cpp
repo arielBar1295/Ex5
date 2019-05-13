aae670463de2d1f347ee16b6fe44151f411adee6
arielBar1295 <44972743+arielBar1295@users.noreply.github.com>
2019-05-13 19:40:01 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -16,14 +16,20 @@ using namespace std;
 #include "chain.hpp"
 #include "zip.hpp"
 #include "product.hpp"
-//#include "powerset.hpp"
+#include "powerset.hpp"
 using namespace itertools;
 #include "badkan.hpp"
 #define COMMA ,
 
 
-
-
+// returns a string
+template<typename Iterable>
+string iterable_to_string( Iterable& iterable) {
+    ostringstream ostr;
+    for (decltype(*iterable.begin()) i: iterable)
+        ostr << i << ","<<" ";
+    return ostr.str();
+}
 int main() {
 
     badkan::TestCase testcase;
@@ -37,230 +43,126 @@ auto r3=range<double>(5.7,11.7);
 auto r4=range<double> (2.3,6.3);
 auto r5=range<char>('a','f');
 auto r6=range<char>('l','z');
-auto r7=range<int>(4,4);
-//for the begin iterator for each range.
-auto Itr1=r1.begin();
-auto Itr2=r1.begin();
-auto Itr3=r3.begin();
-auto Itr4=r4.begin();
-auto Itr5=r5.begin();
-auto Itr6=r6.begin();
-auto Itr7=r7.begin();
-//for saving the answers.
-string answer="";
-string answer1="";
-string answer2="";
-string answer3="";
-string answer4="";
-string answer5="";
-string answer7="";
-for(int i: r1) {
-    answer += to_string(i);
-}
+auto r7=range<int>(4,4);   //a range between a number to itself should return an empty range.
 
-for(int i: r2) {
-    answer1 += to_string(i);
-}
-for(double i: r3) {
-    answer2 += to_string(i);
-}
-for(double i: r4) {
-    answer3 += to_string(i);
-}
-for(char i: r5) {
-    answer4 +=i;
-}
-for(char i: r6) {
-    answer5 +=i;
-}
-for(int i: r7) {
-    answer7 += to_string(i);
-}
+string answer=iterable_to_string(r1);
+string answer1=iterable_to_string(r2);
+string answer2=iterable_to_string(r3);
+string answer3=iterable_to_string(r4);
+string answer4=iterable_to_string(r5);
+string answer5=iterable_to_string(r6);
+string answer7=iterable_to_string(r7);
 
 testcase.setname("range result ")
-.CHECK_EQUAL ( answer=="45678", true)
-.CHECK_EQUAL ( answer1=="45678910", true)
+.CHECK_EQUAL ( answer=="4, 5, 6, 7, 8, ", true)
+.CHECK_EQUAL ( answer1=="4, 5, 6, 7, 8, 9, 10, ", true)
 .CHECK_EQUAL ( answer7=="", true)
-.CHECK_EQUAL ( answer2=="5.7000006.7000007.7000008.7000009.70000010.700000", true)
-.CHECK_EQUAL ( answer3=="2.3000003.3000004.3000005.300000", true)
-.CHECK_EQUAL ( answer4=="abcde", true)
-.CHECK_EQUAL ( answer5=="lmnopqrstuvwxy", true)
+.CHECK_EQUAL ( answer2=="5.7, 6.7, 7.7, 8.7, 9.7, 10.7, ", true)
+.CHECK_EQUAL ( answer3=="2.3, 3.3, 4.3, 5.3, ", true)
+.CHECK_EQUAL ( answer4=="a, b, c, d, e, ", true)
+.CHECK_EQUAL ( answer5=="l, m, n, o, p, q, r, s, t, u, v, w, x, y, ", true)
 ;
-
-//chain test
+//
+////chain test
 auto c1= chain(range('a','f'), string("cppEX5"));
 auto c2= chain(string("arieluni"),string("vercity"));
 auto c3= chain(range('a','f'), range('f','l'));
 auto c4= chain(range(12,20), range(30,40));
 auto c5= chain(range(4.4,8.4), range(7.3,10.3));
-//iterator for each variable.
-auto Itc1=c1.begin();
-auto Itc2=c2.begin();
-auto Itc3=c3.begin();
-auto Itc4=c4.begin();
-auto Itc5=c5.begin();
-//saving the answers
-string answerC1="";
-string answerC2="";
-string answerC3="";
-string answerC4="";
-string answerC5="";
-
-for(char i : c1){
-    answerC1+=i;
-}
-for(char i : c2){
-    answerC2+=i;
-}
-for(char i : c3){
-    answerC3+=i;
-}
-for(int i : c4){
-    answerC4+=to_string(i);
-}
-for(double i : c5){
-    answerC5+=to_string(i);
-}
+string answerC1=iterable_to_string((c1));
+string answerC2=iterable_to_string((c2));
+string answerC3=iterable_to_string((c3));
+string answerC4=iterable_to_string((c4));
+string answerC5=iterable_to_string((c5));
 
         testcase.setname("chain results  ")
-           .CHECK_EQUAL ( answerC1=="abcdecppEX5", true )
-           .CHECK_EQUAL ( answerC2=="arielunivercity", true )
-           .CHECK_EQUAL ( answerC3=="abcdefghijk", true )
-           .CHECK_EQUAL ( answerC4=="121314151617181930313233343536373839", true )
-           .CHECK_EQUAL ( answerC5== "4.4000005.4000006.4000007.4000007.3000008.3000009.300000", true )
+           .CHECK_EQUAL ( answerC1=="a, b, c, d, e, c, p, p, E, X, 5, ", true )
+           .CHECK_EQUAL ( answerC2=="a, r, i, e, l, u, n, i, v, e, r, c, i, t, y, ", true )
+           .CHECK_EQUAL ( answerC3=="a, b, c, d, e, f, g, h, i, j, k, ", true )
+           .CHECK_EQUAL ( answerC4=="12, 13, 14, 15, 16, 17, 18, 19, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, ", true )
+           .CHECK_EQUAL ( answerC5== "4.4, 5.4, 6.4, 7.4, 7.3, 8.3, 9.3, ", true )
                 ;
-//zip test
+////zip test
 auto z1=zip(range(1,6), string("hello"));
 auto z2=zip(range('a','g'), string("heyyou"));
-auto z3=zip(chain(range(1,4), range(4,8)), string("arielba"));
+auto z3=zip(chain(range(1,4), range(4,8)), string("arielba")); //zip which accepts chain.
 auto z4=zip(range(1,6), range(1,6));
 auto z5=zip(range(14,18),string("abcd"));
 auto z6=zip(range(10,14), string("abcd"));
-auto z7= zip(z6,z5);
+auto z7= zip(z6,z5);  //zip which accepts 2 zips.
 auto z8=zip(range(5.1,7.1), range('a','c'));
 auto z9=zip(range(1,3), range('c','e'));
 auto z10=zip(z8,z9);
+string answerZ1=iterable_to_string(z1);
+string answerZ2=iterable_to_string(z2);
+string answerZ3=iterable_to_string(z3);
+string answerZ4=iterable_to_string(z4);
+string answerZ5=iterable_to_string(z5);
+string answerZ6=iterable_to_string(z6);
+string answerZ7=iterable_to_string(z7);
+string answerZ8=iterable_to_string(z8);
+string answerZ9=iterable_to_string(z9);
+string answerZ10=iterable_to_string(z10);
 
-auto Itz1=z1.begin();
-auto Itz2=z2.begin();
-auto Itz3=z3.begin();
-auto Itz4=z4.begin();
-auto Itz5=z5.begin();
-auto Itz6=z6.begin();
-auto Itz7=z7.begin();
-auto Itz8=z8.begin();
-auto Itz9=z9.begin();
-auto Itz10=z10.begin();
-//for saving and compare the answers.
-string answerZ1="";
-string answerZ2="";
-string answerZ3="";
-string answerZ4="";
-string answerZ5="";
-string answerZ6="";
-string answerZ7="";
-string answerZ8="";
-string answerZ9="";
-string answerZ10="";
-
-        for(auto pair  : z1){
-            answerZ1=answerZ1+to_string(pair.first)+pair.second+" ";
-        }
-        for(auto pair  : z2){
-            answerZ2=answerZ2+pair.first+pair.second+" ";
-        }
-        for(auto pair  : z3){
-            answerZ3=answerZ3+to_string(pair.first)+pair.second+" ";
-        }
-        for(auto pair  : z4){
-            answerZ4=answerZ4+to_string(pair.first)+to_string(pair.second)+" ";
-        }
-        for(auto pair  : z5){
-            answerZ5=answerZ5+to_string(pair.first)+pair.second+" ";
-        }
-        for(auto pair  : z6){
-            answerZ6=answerZ6+to_string(pair.first)+pair.second+" ";
-        }
-        for(auto pair  : z7){
-            answerZ7=answerZ7+to_string(pair.first.first)+pair.first.second+to_string(pair.second.first)+pair.second.second+" ";
-        }
-        for(auto pair  : z8){
-            answerZ8=answerZ8+to_string(pair.first)+pair.second+" ";
-        }
-        for(auto pair  : z9){
-            answerZ9=answerZ9+to_string(pair.first)+pair.second+" ";
-        }
-        for(auto pair  : z10){
-            answerZ10=answerZ10+to_string(pair.first.first)+pair.first.second+to_string(pair.second.first)+pair.second.second+" ";
-        }
         testcase.setname("zip results  ")
-                .CHECK_EQUAL ( answerZ1=="1h 2e 3l 4l 5o ", true )
-                .CHECK_EQUAL ( answerZ2=="ah be cy dy eo fu ", true )
-                .CHECK_EQUAL ( answerZ3=="1a 2r 3i 4e 5l 6b 7a ", true )
-                .CHECK_EQUAL ( answerZ4=="11 22 33 44 55 ", true )
-                .CHECK_EQUAL ( answerZ6=="10a 11b 12c 13d ", true )
-                .CHECK_EQUAL ( answerZ5=="14a 15b 16c 17d ", true )
-                .CHECK_EQUAL ( answerZ7==  "10a14a 11b15b 12c16c 13d17d ", true )
-                .CHECK_EQUAL ( answerZ8=="5.100000a 6.100000b ", true )
-                .CHECK_EQUAL ( answerZ9=="1c 2d ", true )
-                .CHECK_EQUAL ( answerZ10=="5.100000a1c 6.100000b2d ", true )
+                .CHECK_EQUAL ( answerZ1=="1,h, 2,e, 3,l, 4,l, 5,o, ", true )
+                .CHECK_EQUAL ( answerZ2=="a,h, b,e, c,y, d,y, e,o, f,u, ", true )
+                .CHECK_EQUAL ( answerZ3=="1,a, 2,r, 3,i, 4,e, 5,l, 6,b, 7,a, ", true )
+                .CHECK_EQUAL ( answerZ4=="1,1, 2,2, 3,3, 4,4, 5,5, ", true )
+                .CHECK_EQUAL ( answerZ6=="10,a, 11,b, 12,c, 13,d, ", true )
+                .CHECK_EQUAL ( answerZ5=="14,a, 15,b, 16,c, 17,d, ", true )
+                .CHECK_EQUAL ( answerZ7==  "10,a,14,a, 11,b,15,b, 12,c,16,c, 13,d,17,d, ", true )
+                .CHECK_EQUAL ( answerZ8=="5.1,a, 6.1,b, ", true )
+                .CHECK_EQUAL ( answerZ9=="1,c, 2,d, ", true )
+                .CHECK_EQUAL ( answerZ10=="5.1,a,1,c, 6.1,b,2,d, ", true )
                 ;
-//product test
+////product test
 auto p1= product(range(1,2), string("hello"));
 auto p2= product(range(1,5), string("h"));
 auto p3= product(string("abcd"), string("h"));
 auto p4= product(string("ab"), range(2.1,4.1));
-auto p5= product(chain(range(1,4),range(10,12)),range('a','c'));
+auto p5= product(chain(range(1,4),range(10,12)),range('a','c')); //product with chain and range
 auto p6= product(range(5,5), range(2,2));
 auto p7= product(range('a','d'), range('d','f'));
+string answerP1=iterable_to_string(p1);
+string answerP2=iterable_to_string(p2);
+string answerP3=iterable_to_string(p3);
+string answerP4=iterable_to_string(p4);
+string answerP5=iterable_to_string(p5);
+string answerP6=iterable_to_string(p6);
+string answerP7=iterable_to_string(p7);
 
-auto Itp1=p1.begin();
-auto Itp2=p2.begin();
-auto Itp3=p3.begin();
-auto Itp4=p4.begin();
-auto Itp5=p5.begin();
-auto Itp6=p6.begin();
-auto Itp7=p7.begin();
-
-string answerP1="";
-string answerP2="";
-string answerP3="";
-string answerP4="";
-string answerP5="";
-string answerP6="";
-string answerP7="";
-        for(auto pair  : p1){
-            answerP1=answerP1+to_string(pair.first)+pair.second+" ";
-        }
-        for(auto pair  : p2){
-            answerP2=answerP2+to_string(pair.first)+pair.second+" ";
-        }
-        for(auto pair  : p3){
-            answerP3=answerP3+pair.first+pair.second+" ";
-        }
-        for(auto pair  : p4){
-            answerP4=answerP4+pair.first+to_string(pair.second)+" ";
-        }
-        for(auto pair  : p5){
-            answerP5=answerP5+to_string(pair.first)+pair.second+" ";
-        }
-        for(auto pair  : p6){
-            answerP6=answerP6+to_string(pair.first)+to_string(pair.second)+" ";
-        }
-        for(auto pair  : p7){
-            answerP7=answerP7+pair.first+pair.second+" ";
-        }
         testcase.setname("product results  ")
-                .CHECK_EQUAL ( answerP1=="1h 1e 1l 1l 1o ", true )
-                .CHECK_EQUAL ( answerP2=="1h 2h 3h 4h ", true )
-                .CHECK_EQUAL ( answerP3=="ah bh ch dh ", true )
-                .CHECK_EQUAL ( answerP4=="a2.100000 a3.100000 b2.100000 b3.100000 ", true )
-                .CHECK_EQUAL ( answerP5=="1a 1b 2a 2b 3a 3b 10a 10b 11a 11b ", true )
+                .CHECK_EQUAL ( answerP1=="1,h, 1,e, 1,l, 1,l, 1,o, ", true )
+                .CHECK_EQUAL ( answerP2=="1,h, 2,h, 3,h, 4,h, ", true )
+               .CHECK_EQUAL ( answerP3=="a,h, b,h, c,h, d,h, ", true )
+                .CHECK_EQUAL ( answerP4=="a,2.1, a,3.1, b,2.1, b,3.1, ", true )
+                .CHECK_EQUAL ( answerP5=="1,a, 1,b, 2,a, 2,b, 3,a, 3,b, 10,a, 10,b, 11,a, 11,b, ", true )
                 .CHECK_EQUAL ( answerP6=="", true )
-                .CHECK_EQUAL ( answerP7=="ad ae bd be cd ce ", true )
+                .CHECK_EQUAL ( answerP7=="a,d, a,e, b,d, b,e, c,d, c,e, ", true )
+               ;
+//
+////powerset tests.
+auto ps= powerset(string("he"));
+auto ps1= powerset(range(2.2,5.2));
+auto ps2= powerset(range('e','i'));
+auto ps3= powerset(range(10,14));
+auto ps4= powerset(chain(range(1,3),range(5,7)));  //powerset that accepts chain with 2 ranges of int.
+auto ps5= powerset(string("ariel"));
+string answerPs=iterable_to_string(ps);
+string answerPs1=iterable_to_string(ps1);
+string answerPs2=iterable_to_string(ps2);
+string answerPs3=iterable_to_string(ps3);
+string answerPs4=iterable_to_string(ps4);
+string answerPs5=iterable_to_string(ps5);
+        testcase.setname("powerset results  ")
+                .CHECK_EQUAL ( answerPs=="{}, {h}, {e}, {he}, ", true )
+                .CHECK_EQUAL ( answerPs1=="{}, {2.200000}, {3.200000}, {2.2000003.200000}, {4.200000}, {2.2000004.200000}, {3.2000004.200000}, {2.2000003.2000004.200000}, ", true )
+                .CHECK_EQUAL ( answerPs2=="{}, {e}, {f}, {ef}, {g}, {eg}, {fg}, {efg}, {h}, {eh}, {fh}, {efh}, {gh}, {egh}, {fgh}, {efgh}, ", true )
+                .CHECK_EQUAL ( answerPs3=="{}, {10}, {11}, {1011}, {12}, {1012}, {1112}, {101112}, {13}, {1013}, {1113}, {101113}, {1213}, {101213}, {111213}, {10111213}, ", true )
+                .CHECK_EQUAL ( answerPs4=="{}, {1}, {2}, {12}, {5}, {15}, {25}, {125}, {6}, {16}, {26}, {126}, {56}, {156}, {256}, {1256}, ", true )
+                .CHECK_EQUAL ( answerPs5=="{}, {a}, {r}, {ar}, {i}, {ai}, {ri}, {ari}, {e}, {ae}, {re}, {are}, {ie}, {aie}, {rie}, {arie}, {l}, {al}, {rl}, {arl}, {il}, {ail}, {ril}, {aril}, {el}, {ael}, {rel}, {arel}, {iel}, {aiel}, {riel}, {ariel}, ", true )
                 ;
-
-//powerset tests.
 
         grade = testcase.grade();
     } else {
@@ -270,3 +172,5 @@ string answerP7="";
     cout << "Your grade is: "  << grade << endl;
     return 0;
 }
+
+
