f1e627a577153cf0fcdc1960dd56a069cb5447b9
arielBar1295 <44972743+arielBar1295@users.noreply.github.com>
2019-05-26 08:11:34 +0300

    Update powerset.hpp

--- powerset.hpp
+++ powerset.hpp
@@ -74,7 +74,7 @@ namespace itertools {
 //                            bool isC2=false;
 //                              if(*tStart.first>='a' && *tStart.first<='z') {
 //                                    isC = true;
-                             ans=ans + *tStart;
+                             ans=ans + to_string(*tStart);
                          }
 
 //                       }
